c5a8b0ee23f7a1de609cd3b64d5a0c8250ad0e01
Rainald Balog <rainald.balog@arobs.com>
2020-01-29 00:38:10 -0800

    added token auth for login and a verify token function

---
+++ src/controllers/verifyToken.js
@@ -0,0 +1,22 @@
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const db = require('../database');
+
+module.exports = (req, res, next) => {
+  // check header or url parameters or post parameters for token
+  var token = req.headers['x-access-token'];
+  if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
+
+  // verifies secret and checks exp
+  // HARDCODE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  jwt.verify(token, 'config.secret', async function(err, decoded) {
+    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+    // if everything is good, save to request for use in other routes
+    const user = await db.get('SELECT * FROM users where users.id = $id', {
+      $id: decoded.id,
+    });
+    if (!user) return res.status(404).send('No user found.');
+    req.userId = decoded.id;
+    next();
+  });
+};
--- src/init/route.js
+++ src/init/route.js
@@ -1,6 +1,8 @@
 const gameController = require('../controllers/game');
 const userController = require('../controllers/user');
 const scoreController = require('../controllers/score');
+const authController = require('../controllers/authController');
+const verifyToken = require('../controllers/verifyToken');
 
 module.exports = (app) => {
   app.get('/games', gameController.get);
@@ -8,8 +10,13 @@ module.exports = (app) => {
   app.get('/games/:id', gameController.getById);
   app.get('/users/:id', userController.getById);
   app.get('/scores/:id', scoreController.getById);
+  app.get('/logout', authController.logout);
+
+  // if you want to check, user is logged in then write like this:
+  // app.get('/games/:id', verifyToken, gameController.getById);
 
   app.post('/users', userController.post);
   app.post('/games', gameController.post);
   app.post('/scores', scoreController.post);
+  app.post('/login', authController.login);
 };
