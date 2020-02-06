810c8fd956ccd71eac3db5bb9f0c8d1a15511eb3
Balog Rajnald <rajnald94@gmail.com>
2020-02-06 15:39:41 +0200

    added description for Picture Match game

--- src/database/inserts.sql
+++ src/database/inserts.sql
@@ -2,7 +2,7 @@ begin transaction;
 INSERT INTO games(name, imageUrl, description) 
 VALUES 
 ('snake','images/snake.png',"test description lorem ipsum dolor sit amet n"),
-('picturematch','images/picturematch.png',"test description lorem ipsum dolor sit amet n"),
+('Picture Match','images/picturematch.png',"A game that will improve your memory and brain power. A board with several pairs of disordered images. You will have to find all the couples. No time limit and 14 levels to which more difficult."),
 ('tictactoe','images/tictactoe.png',"test description lorem ipsum dolor sit amet n"),
 ('hangman','images/hangman.png',"test description lorem ipsum dolor sit amet n"),
 ('battleship','images/battleship.png',"test description lorem ipsum dolor sit amet n"),
