94f659baf002e016354156ad29890b4694badc54
hchris21 <christopher.horn@arobs.com>
2020-02-07 10:48:56 +0200

    finalchange

--- src/database/inserts.sql
+++ src/database/inserts.sql
@@ -1,15 +1,9 @@
 begin transaction;
 INSERT INTO games(name, imageUrl, description) 
 VALUES 
-<<<<<<< HEAD
 ('snake','images/snake.png',"My pet snake is exactly 3.14 metres long. He’s a πthon."),
 ('picturematch','images/picturematch.png',"A game that will improve your memory and brain power. A board with several pairs of disordered images. You will have to find all the couples. No time limit and 14 levels to which more difficult."),
 ('tictactoe','images/tictactoe.png',"Why can one artist never beat another artist at tic tac toe? They always end up drawing"),
-=======
-('snake','images/snake.png',"test description lorem ipsum dolor sit amet n"),
-('Picture Match','images/picturematch.png',"A game that will improve your memory and brain power. A board with several pairs of disordered images. You will have to find all the couples. No time limit and 14 levels to which more difficult."),
-('tictactoe','images/tictactoe.png',"test description lorem ipsum dolor sit amet n"),
->>>>>>> 810c8fd956ccd71eac3db5bb9f0c8d1a15511eb3
 ('hangman','images/hangman.png',"test description lorem ipsum dolor sit amet n"),
 ('battleship','images/battleship.png',"You missed! You hit me! You sunk me!"),
 ('mathquiz', 'images/mathquiz.png', "Two plus two is four minus one that's three quick maths"),
